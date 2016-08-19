e9cf1cb5b2c70c5b7a61d87f686ef0e1c74d5da5
JoeZwet <zwet.joe@gmail.com>
2016-08-19 23:13:56 +1200

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,11 @@
+<!--
+All eode is under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007.
+-->
+
 <?php
 $db_host = "localhost";  // server to connect to.
-$db_name = "mydata";  // the name of the database.
-$db_user = "myusername";  // mysql username to access the database with.
-$db_pass = "mypassword";  // mysql password to access the database with.
+$db_name = "login";  // the name of the database.
+$db_user = "database username";  // mysql username to access the database with.
+$db_pass = "database password";  // mysql password to access the database with.
 $db_table = "users";    // the table that this script will set up and use.
 ?>

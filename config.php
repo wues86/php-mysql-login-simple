28d596f2bcf71f92fc61d5934d107f5c0b163907
JoeZwet <zwet.joe@gmail.com>
2016-08-19 22:22:18 +1200

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$db_host = "localhost";  // server to connect to.
+$db_name = "mydata";  // the name of the database.
+$db_user = "myusername";  // mysql username to access the database with.
+$db_pass = "mypassword";  // mysql password to access the database with.
+$db_table = "users";    // the table that this script will set up and use.
+?>
